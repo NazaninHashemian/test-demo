48f04c8f345bba2c32b4772ce8d621eb23dc4679
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-06-28 00:22:06 -0700

    remove Counter.test comments

--- src/Counter.test.jsx
+++ src/Counter.test.jsx
@@ -3,8 +3,6 @@ import Counter from "./Counter";
 
 test("initial counter is 0", () => {
     render(<Counter />);
-    //   const countElement = screen.getByText("Count: 0");
-    // const countElement = screen.getByText((content) => content.includes("0"));
     const countElement = screen.getByText("Current count: 0");
     expect(countElement).toBeInTheDocument();
   });
@@ -13,8 +11,6 @@ test("initial counter is 0", () => {
     render(<Counter />);
     const button = screen.getByText("Increase");
     fireEvent.click(button);
-    //   const countElement = screen.getByText("Count: 1");
-    // const countElement = screen.getByText((content) => content.includes("1"));
     const countElement = screen.getByText("Current count: 1");
     expect(countElement).toBeInTheDocument();
   });
